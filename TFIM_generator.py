40af647b646e3e3b73c720d07d0b19e866aee5bd
paulbruzzi <paulbruzzi@gmail.com>
2025-03-28 17:38:13 -0400

    added testing for method

--- TFIM_generator.py
+++ TFIM_generator.py
@@ -29,7 +29,7 @@ def generate_TFIM_gates(qubits, steps, dt, g, location):
         f.write("[Jy]\nvalue = 0\n\n")
         f.write("[Jz]\nvalue = 1\n\n")
         f.write("[hx]\nramp = constant\nvalue = "+str(g)+"\n\n")
-        f.write("[Output]\nname = TFIM_Operators/n="+str(qubits)+"_g="+str(g)+"_dt="+str(dt)+"_i=\nimin = 1\nimax = "+str(steps)+"\nstep = 1\n")
+        f.write("[Output]\nname = TFIM_Operators/n="+str(qubits)+"_g="+str(g)+"_dt="+str(dt)+"_i=\nimin = 1\nimax = "+str(steps+1)+"\nstep = 1\n")
     exe = location+"/release/examples/f3c_time_evolution_TFYZ"
     subprocess.run([exe, "TFIM_Operators/Operator_Generator.ini"])
     os.remove("TFIM_Operators/Operator_Generator.ini")
@@ -39,3 +39,47 @@ def generate_TFIM_gates(qubits, steps, dt, g, location):
         os.remove("TFIM_Operators/n="+str(qubits)+"_g="+str(g)+"_dt="+str(dt)+"_i="+str(step+1)+".qasm")
     os.rmdir("TFIM_Operators")
     return gates
+
+# from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+# from qiskit.circuit.library import QFT
+# from qiskit_aer import AerSimulator
+# from numpy import pi
+
+# particles = 2
+# Dt = .1
+# gates = [generate_TFIM_gates(particles, int(2*pi/Dt), Dt, 10, '../f3cpp')[1]]
+
+# s_k =[]
+# for controlled_U in gates:
+#     bits = 20
+#     qr_ancilla = QuantumRegister(bits)
+#     qr_eigen   = QuantumRegister(particles)
+#     cr         = ClassicalRegister(bits)
+
+#     qc = QuantumCircuit(qr_ancilla, qr_eigen, cr)
+#     qc.h(qr_ancilla)
+#     qc.h(qr_eigen)
+#     qc.barrier()
+    
+#     for i in range(len(qr_ancilla)):
+#         for _ in range(i):
+#             qc.append(controlled_U, qargs=[qr_ancilla[i]]+qr_eigen[:] )
+#     qc.append(QFT(bits, inverse = True), qr_ancilla)
+#     for i in range(len(qr_ancilla)): qc.measure(qr_ancilla[i], cr[i])
+#     # print(qc)
+#     aer_sim = AerSimulator()
+#     trans_qc = transpile(qc, aer_sim)
+#     counts = aer_sim.run(trans_qc, shots = 10**4).result().get_counts()
+#     binary_num = ''
+#     max_num = 0
+#     for key in counts:
+#         if (counts[key] > max_num):
+#             max_num = counts[key]
+#             binary_num = key
+#     # print("Binary Number with Maximum count:     0."+binary_num)
+#     decimal_num = 0
+#     for i in range(len(binary_num)):
+#         decimal_num += int(binary_num[i])/2**(i+1)
+#     s_k.append(decimal_num)
+# print(s_k)
+
